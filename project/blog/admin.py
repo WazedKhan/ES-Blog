c8f0bfdf7461b880ad0a8b02d8a7028641fc8759
WazedKhan-Repliq <wazed.khan@repliq.dev>
2023-08-19 12:14:14 +0600

    commands

--- project/blog/admin.py
+++ project/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, BlogPost, Reaction, Comment
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ["title", "content", "author", "date_published"]
